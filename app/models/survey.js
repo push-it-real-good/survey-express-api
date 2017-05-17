e2d0ded49c5fe1b642d7f24e902aeceeeececece
kqngo <khan.ngo@gmail.com>
2017-05-17 14:36:41 -0400

    Add question to survey model

--- app/models/survey.js
+++ app/models/survey.js
@@ -7,6 +7,10 @@ const surveySchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  questions: {
+    type: String,
+    required: true
+  },
   _owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
