7ab8e57b38fcb63948201529511e51a67af52d33
kqngo <khan.ngo@gmail.com>
2017-05-17 16:01:59 -0400

    Update destroy method in the response.js controller and tested

--- app/controllers/responses.js
+++ app/controllers/responses.js
@@ -64,7 +64,8 @@ const update = (req, res, next) => {
 
 const destroy = (req, res, next) => {
   // remove response from db
-  req.response.remove()
+  // req.response.remove()
+  Response.find({'survey_id': req.body.survey_id}).remove()
   // if successfully remove ex from db, return 204 to client
     .then(() => res.sendStatus(204))
     // error handling
@@ -81,6 +82,7 @@ module.exports = controller({
   // runs the setModel middleware for show controller action
   { method: setModel(Response), only: ['show'] },
   // runs the setModel middleware for update and destroy controller actions, making sure the user only does it on things associated with that user
-  { method: setModel(Response, { forUser: true }), only: ['update', 'destroy'] }
+  // { method: setModel(Response, { forUser: true }), only: ['update', 'destroy'] }
+  { method: setModel(Response, { forUser: true }), only: ['update'] }
 ]
 })
--- scripts/responses/destroy.sh
+++ scripts/responses/destroy.sh
@@ -5,11 +5,16 @@ API="http://localhost:4741"
 URL_PATH="/responses"
 
 TOKEN="dUim9y/Jlt3mDMwPo+qs1yzy+5eJJlFNNiq/kM8ZMt0=--RFecLpZ+BWQvM6kBH0UecTypOadd2ZOfYrm0iBQGsxc="
-ID="591ca14bea0091c8a3783eb8"
 
-curl "${API}${URL_PATH}/${ID}" \
+SURVEYID="591ca14bea0091c8a3783eb8"
+
+curl "${API}${URL_PATH}" \
   --include \
   --request DELETE \
-  --header "Authorization: Token token=${TOKEN}"
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+      "survey_id": "'"${SURVEYID}"'"
+    }'
 
 echo
